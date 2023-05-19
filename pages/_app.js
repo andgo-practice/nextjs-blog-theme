ab564e4d527c925a7fe31474d50958c7b96c7815
bumbums <bumbums@webtoonscorp.com>
2023-05-19 14:36:34 +0900

    aaa

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 function MyApp({ Component, pageProps }) {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const socket = io();
+    setSocket(socket);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <span className="theme-bejamas" />
-      <Component {...pageProps} />
+      <Component {...pageProps} socket={socket} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
-
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
@@ -8,54 +8,19 @@ import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
-export default function Index({ posts, globalData }) {
+function Index({ posts, globalData, socket }) {
+  useEffect(() => {
+    if (socket) {
+      socket.on('imageCaptured', (imageFileName) => {
+        // 이미지를 HTML에 추가하는 로직 작성
+      });
+    }
+  }, [socket]);
+
+  // 나머지 코드 작성
+
   return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
+    // 나머지 JSX 코드 작성
   );
 }
 
@@ -65,3 +30,5 @@ export function getStaticProps() {
 
   return { props: { posts, globalData } };
 }
+
+export default Index;
